7a90bc474544f6af5c67a17f425bfb0f68c4d04f
ESM Smart Solutions <esm.smart.solutions@outlook.com>
2019-11-22 12:18:15 -0500

    Add project files.

---
+++ XPA_GetContacts/XPA_GetContacts/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XPA_GetContacts
+{
+    public partial class App : Application
+    {
+        public const string AppNameAcronym = "Get Contacts";
+        public static string appVersion = "1.0";
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        internal static void SetAppTitleAndToday(Label lblAppTitle, Label lblTodayIs)
+        {
+            lblAppTitle.Text = $"{App.AppNameAcronym} {App.appVersion}";
+            DateTime today = DateTime.Today;
+            string dtSuffix = "";
+            switch (today.Day)
+            {
+                case 1:
+                    dtSuffix = "st";
+                    break;
+                case 2:
+                    dtSuffix = "nd";
+                    break;
+                case 3:
+                    dtSuffix = "rd";
+                    break;
+                default:
+                    dtSuffix = "th";
+                    break;
+            }
+            lblTodayIs.Text = $"{today.ToString("MMM")} {today.Day}{dtSuffix}, {today.Year}";
+        }
+    }
+}
---
+++ XPA_GetContacts/XPA_GetContacts/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XPA_GetContacts
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        IList<Plugin.ContactService.Shared.Contact> contacts;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            App.SetAppTitleAndToday(this.AppTitle, this.TodayIs);
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            contacts = await Plugin.ContactService.CrossContactService.Current.GetContactListAsync();
+            lvSearchedContacts.ItemsSource = contacts;
+        }
+
+        private void sbSearchContacts_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lvSearchedContacts.ItemsSource = contacts.Where(x=>(x.Numbers.Count > 0 || x.Emails.Count > 0) && x.Name.Contains(sbSearchContacts.Text));
+        }
+
+        private void lvSearchedContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Plugin.ContactService.Shared.Contact contact = (Plugin.ContactService.Shared.Contact)lvSearchedContacts.SelectedItem;
+
+            // Populate phone numbers list
+            List<string> numbers = new List<string>();
+
+            foreach(string strNumber in contact.Numbers)
+            {
+                string[] n = strNumber.Split('=');
+
+                numbers.Add($"{(n[2].Contains("null") ? "" : n[2])}{n[1].Split(',')[0]}");
+            }
+
+            lvNumbers.ItemsSource = numbers;
+
+            // Populate E-Mails list
+            lvEmails.ItemsSource = contact.Emails;
+        }
+    }
+}
